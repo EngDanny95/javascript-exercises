60d330cc0d4ff24fd797bc3b198ba4b84d1b4318
Danny <EngDanny95@gmail.com>
2023-08-18 23:36:07 +0300

    fix(reverse-string): reverse all kinds of strings

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
-
-};
-
+let word = '';
+const reverseString = function (string) {
+    for (i = string.length - 1; i >= 0; i--) {
+        let rev = string[i];
+        word += rev;
+    };
+    return word;
+}
 // Do not edit below this line
 module.exports = reverseString;
