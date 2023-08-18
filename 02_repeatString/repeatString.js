a7e2e6967e8bac338856bc6056b4834730c3ae4e
Danny <EngDanny95@gmail.com>
2023-08-18 22:26:09 +0300

    fix(repeat-string):repeat string n times

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(s,n) {
+   let l = s;
+   const number = Math.floor(Math.random()*1000);
+   if(n==0){
+    s='';} 
+    else if(n<0){
+        return 'ERROR'
+    }
+   for (let i=1; i<n;i++){
+        s += l;
+    }
+    return s;
 };
 
 // Do not edit below this line
