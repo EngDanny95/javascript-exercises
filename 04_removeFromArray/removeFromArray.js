aaad23f1104a14fd0a0f12704c1dad06310123dc
Danny <EngDanny95@gmail.com>
2023-08-24 23:11:12 +0300

    build(func):add string or number remove from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arrayNumbers, ...nums) { 
+//using Rest argument, the function can go through any number of parameters added
+    for (i=0;i<arrayNumbers.length;i++){ 
+        for (let n of nums){
+        if (arrayNumbers[i]===n){
+            arrayNumbers.splice(i,1);
+            i--;
+        }}
+    }
+    return arrayNumbers;
 };
+
+//removeFromArray([1,2,3,4],2,3,1,4)
 
 // Do not edit below this line
 module.exports = removeFromArray;
