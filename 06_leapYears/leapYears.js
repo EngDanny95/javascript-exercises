467d1fd5e991b429d5f87f628a4870872a4dde60
Danny <EngDanny95@gmail.com>
2023-08-25 21:41:55 +0300

    build(func): determine leap years

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,18 @@
-const leapYears = function() {
+const leapYears = function (leap) {
+//start if statement with the most specific exception before going with the more common results 
+    if (!(leap % 400)) {
+        return true;
+    }
+    else if (!(leap % 100)) {
+        return false;
+    }
+    else if (!(leap % 4)) {
+        return true;
+    }
+
+    else {
+        return false;
+    }
 
 };
 
