a678a6bfd0c239b3576a04c3d4939b80fd857ed1
Danny <EngDanny95@gmail.com>
2023-08-25 21:15:58 +0300

    build(func):add range number between inputs

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+    let c = b
+    if (a > b) {
+        b = a;
+        a = c;
+    }
+    let i = a;
+    let sum = 0;
+    if (typeof (a) === 'string' || typeof (b) === 'string') {
+        return 'ERROR'
+    }
 
+    else if (a >= 0 && b >= 0) {
+        console.log(a, b)
+        while (i <= b) {
+            sum += i;
+            i++
+        }
+        return sum;
+    }
+
+    else{
+        return 'ERROR'
+    }
 };
 
 // Do not edit below this line
